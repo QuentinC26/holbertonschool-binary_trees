5d9049b27b32e8c91cb4f8681a6e25a42ccf4a70
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-04-08 17:22:57 +0200

    add the file 0-binary_tree_node.c and test my code.

--- binary_trees.h
+++ binary_trees.h
@@ -27,5 +27,6 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
