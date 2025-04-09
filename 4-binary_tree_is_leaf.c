9aecb1a9f5da5c377186d3f383a6c62b55485144
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-09 10:37:27 +0200

    add two condition if and different return

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - function for check the leaf
+ * @node: Pointer to the check the node
+ *
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
