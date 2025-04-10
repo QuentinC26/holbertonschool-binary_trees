a41a89ef4608628d703f6351ca7c82cbf9124995
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-10 10:55:11 +0200

    add a four function for compare tree left and tree right

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,69 @@
+#include "binary_trees.h"
+
+/**
+ * maximum - function that compare two integers
+ * @return_left: the integer who will take the left side
+ * @return_right: the integer who will take the right side
+ * Return: return_left or return_right
+ */
+
+int maximum(int return_left, int return_right)
+{
+	if (return_left >= return_right)
+	{
+		return (return_left);
+	}
+	else
+	{
+	return (return_right);
+	}
+}
+
+/**
+ * height - function that count the height of binary tree
+ * @tree: the binary tree
+ * Return: height tree->left ou height tree->right
+ */
+
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (maximum(height(tree->left), height(tree->right)) + 1);
+}
+
+/**
+ * binary_tree_height - function that count the height of binary tree
+ * @tree: the binary tree
+ * Return: height of the tree
+ */
+
+int binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (height(tree));
+}
+
+/**
+ * binary_tree_is_perfect - function that check the perfect binary tree
+ * @tree: the binary tree
+ * Return: perfect 1 or 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (height(tree->left) == height(tree->right))
+	{
+		return (1);
+	}
+	return (0);
+}
